e4515501aa69934a5f8701316f7ffb018bd997ba
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-12 12:25:20 +0100

    fixed some stuff, added basic chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import ChatMain from "../components/ChatMain";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Test</h1>
+      <ChatMain />
     </main>
   );
 }
--- src/components/ChatMain.tsx
+++ src/components/ChatMain.tsx
@@ -1,52 +1,23 @@
 "use client";
 
-import { useChat, Message as AIMessage } from "ai/react";
-import Input
-import { v4 as uuidv4 } from "uuid";
+import { useChat } from "ai/react";
 
-import Button
+
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 import Icon from "./ui/Icon";
 
-import { Chat, Message, Profile } from "@/types/db";
 import { useRef } from "react";
-import { addMessage } from "@/functions/db/messages";
 
-export default function ChatMain({ chat, initMessages, user } : { chat: Chat, initMessages: Message[], user: Profile }) {
+export default function ChatMain() {
     const { messages, input, handleInputChange, handleSubmit } = useChat({
-        initialMessages: initMessages.map((m) => {
-            return {
-                id: m.id,
-                createdAt: m.created_at,
-                content: m.content,
-                role: m.from_ai ? 'assistant' : 'user',
-                
-            } as AIMessage
-        }),
         initialInput: "",
         keepLastMessageOnError: true,
-        body: {
-            profile: user,
-            chat: chat
-        },
         onFinish: async (message, options) => {
             console.log("Finished", message, options);
             if (inputRef.current) {
                 inputRef.current.focus();
             }
-            // add message to db
-            const newMessage: Message = {
-                id: uuidv4(),
-                chat: chat,
-                character: chat.character,
-                user: user,
-                from_ai: message.role === 'user',
-                content: message.content,
-                is_edited: false,
-                is_deleted: false,
-            }
-
-            const res = await addMessage(newMessage);
-            console.log(res);
         }
     });
 
@@ -68,20 +39,10 @@ export default function ChatMain({ chat, initMessages, user } : { chat: Chat, in
             <form className="w-full" onSubmit={handleSubmit}>
                 <Input 
                     placeholder="Send a message" 
-                    size="lg" 
-                    radius="full" 
                     value={input}
                     ref={inputRef}
                     name="prompt"
                     onChange={handleInputChange}
-                    classNames={{
-                        inputWrapper: "pr-1",
-                    }}
-                    endContent={
-                        <Button type="submit" color="secondary" radius="full" isIconOnly>
-                            <Icon filled>send</Icon>
-                        </Button>
-                    } 
                 />
             </form>
         </div>
