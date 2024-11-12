b0493b4809bebad8b78e73960f0fc8ca6ae1af08
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-12 12:02:51 +0100

    Added bunch of packages

---
+++ src/components/ui/Icon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "@material-symbols/font-400";
+
+export default function Icon(
+    { children, color="inherit", darkColor, filled, upscale, downscale } : 
+    { children: React.ReactNode, color?: string, darkColor?: string; filled?: boolean, upscale?: boolean, downscale?: boolean }) {
+    return (
+        <span 
+            className={
+                "material-symbols-rounded text-tiny " 
+                + (color ? ` text-${color} ` : "")
+                + (darkColor ? ` dark:text-${darkColor} ` : "") 
+                + (filled ? " material-symbols-filled " : "") 
+                + (upscale ? " scale-125 " : "") + (downscale ? " scale-75 " : "")
+            }
+        >
+            {children}
+        </span>
+    )
+}
